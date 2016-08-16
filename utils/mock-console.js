436e1e62cdd9efdcf5724c544d93d2e54aa80fc6
Ken Wheeler <ken_wheeler@me.com>
2016-08-16 12:21:28 -0400

    Improving console output

--- utils/mock-console.js
+++ utils/mock-console.js
@@ -1,10 +1,20 @@
 "use strict";
 
+function colorizeAndConcat(handler, color) {
+  var args = Array.prototype.slice.call(arguments, 2);
+  var value = "{" + color + "-fg}";
+  args.forEach(function(arg) {
+    value += arg + " ";
+  });
+  value += "{/}\n";
+  handler(value);
+}
+
 module.exports = function(handler) {
   return {
-    log: handler,
-    info: handler,
-    error: handler,
-    warn: handler
+    log: colorizeAndConcat.bind(null, handler, "white"),
+    info: colorizeAndConcat.bind(null, handler, "white"),
+    error: colorizeAndConcat.bind(null, handler, "red"),
+    warn: colorizeAndConcat.bind(null, handler, "yellow")
   };
-}
+};
