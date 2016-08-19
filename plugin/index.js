8be6a5ac883d1c439e9845f4514b22c491a9c914
WibblyWobbly <simon.radionov@gmail.com>
2016-08-19 17:13:06 +0300

    Add time information to Operation block

--- plugin/index.js
+++ plugin/index.js
@@ -17,8 +17,17 @@ function DashboardPlugin(options) {
   }
 }
 
+function getCurrentTime() {
+  return parseInt((new Date()).getTime() / 1000, 10);
+}
+
+function getTimeMessage(timer) {
+  return ' (' + (getCurrentTime() - timer) + 's)';
+}
+
 DashboardPlugin.prototype.apply = function(compiler) {
   var handler = this.handler;
+  var timer;
 
   if (!handler) {
     handler = noop;
@@ -40,11 +49,12 @@ DashboardPlugin.prototype.apply = function(compiler) {
       value: percent
     }, {
       type: "operations",
-      value: msg
+      value: msg + getTimeMessage(timer)
     }]);
   }));
 
   compiler.plugin("compile", function() {
+    timer = getCurrentTime();
     handler.call(null, [{
       type: "status",
       value: "Compiling"
@@ -75,7 +85,7 @@ DashboardPlugin.prototype.apply = function(compiler) {
       value: 0
     }, {
       type: "operations",
-      value: "idle"
+      value: "idle" + getTimeMessage(timer)
     }, {
       type: "stats",
       value: {
@@ -92,7 +102,7 @@ DashboardPlugin.prototype.apply = function(compiler) {
       value: "Failed"
     }, {
       type: "operations",
-      value: "idle"
+      value: "idle" + getTimeMessage(timer)
     }]);
   });
 
