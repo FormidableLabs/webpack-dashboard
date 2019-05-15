a1187c155784a680948d52d77b993ef9b2328fdc
Ryan Roemer <ryan.roemer@formidable.com>
2019-05-14 20:47:38 -0700

    Fix tests

--- test/utils/format-modules.spec.js
+++ test/utils/format-modules.spec.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { normalize } = require("path");
+const { normalize, sep } = require("path");
 const { _formatFileName, _formatPercentage } = require("../../utils/format-modules");
 
 describe("format-modules", () => {
@@ -9,7 +9,7 @@ describe("format-modules", () => {
       const mod = {
         fileName: normalize("foo/bar/test.js")
       };
-      expect(_formatFileName(mod)).to.equal("{green-fg}./foo/bar/test.js{/}");
+      expect(_formatFileName(mod)).to.equal(`{green-fg}.${sep}foo${sep}bar${sep}test.js{/}`);
     });
 
     context("when there is a baseName", () => {
@@ -28,7 +28,9 @@ describe("format-modules", () => {
           fileName: normalize("/node_modules/@foo/test.js"),
           baseName: normalize("/home/bar/node_modules/@foo/test.js")
         };
-        expect(_formatFileName(mod)).to.equal("~/{yellow-fg}@foo{/}/{yellow-fg}test.js{/}");
+        expect(_formatFileName(mod)).to.equal(
+          `~${sep}{yellow-fg}@foo{/}${sep}{yellow-fg}test.js{/}`
+        );
       });
     });
   });
