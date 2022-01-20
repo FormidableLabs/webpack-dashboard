aaf47797fdcd580c7e895a4d89185ff4ae04ac46
Ryan Roemer <ryan.roemer@formidable.com>
2022-01-20 13:25:46 -0800

    Move TS config to examples and add dev-ts task

--- examples/config/webpack.config.ts
+++ examples/config/webpack.config.ts
@@ -7,13 +7,21 @@ import DashboardPlugin from '../../plugin';
 import * as path from 'path';
 import * as webpack from 'webpack';
 
+const cwd = `${process.cwd()}/examples/${process.env.EXAMPLE}`;
+if (!process.env.EXAMPLE) {
+  throw new Error("EXAMPLE is required");
+}
+
 const config: webpack.Configuration = {
   mode: 'development',
-  entry: './foo.js',
-  context: path.resolve(__dirname, "../../examples/simple"),
+  entry: {
+    bundle: "./src/index.js"
+  },
+  context: path.resolve(cwd),
   output: {
-    path: path.resolve(__dirname, '../../examples/dist-ts-example'),
-    filename: "./src/index.js",
+    path: path.resolve(cwd, 'dist-ts-example'),
+    pathinfo: true,
+    filename: "[name].js"
   },
   devtool: false,
   plugins: [
